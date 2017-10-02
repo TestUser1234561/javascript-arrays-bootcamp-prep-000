655167cfa9c0d30c5aeaf6dd8cbc10a4c894f888
Morgan Fudge <mfudge92@gmail.com>
2017-10-02 19:59:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,4 +5,7 @@ function addElementToBeginningOfArray(array, element) {
   return array;
 }
 
-function destructivelyAddElementToBeginningOfArray(...array, element)
+function destructivelyAddElementToBeginningOfArray(...array, element) {
+  array.unshift(element)
+  return array;
+}
